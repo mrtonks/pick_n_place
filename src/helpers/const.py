Fix angles and issue with quaternion y

src/helpers/const.py
@@ -17,7 +17,7 @@ LIMB = 'right'
 # Object heigth in meters
 OBJECTS = {
     'black_trainer': 0.08,
-    'catbus': 0.05,
+    'catbus': 0.06,
     'cat_cup': 0.10,
     'feet_spray': 0.04,
     'harrogate_water': 0.04,
@@ -87,14 +87,14 @@ CAMERA_MATRIX = np.array([
 DIST_COEFF = np.array([-0.145259, -0.035861, -0.009946, -0.033134, 0.000000], dtype=np.float32)
 
 IMAGE_POINTS = np.array([
-    (382.516, 275.24),  # Upper left
-    (305.355, 630.853),  # Lower left
-    (952.839, 278.595),  # Upper right
-    (1033.35, 630.853)  # Lower right
+    (407.677, 271.147),  # Upper left (x, y)
+    (322.129, 626.097),  # Lower left
+    (978.0, 285.581),  # Upper right
+    (1050.13, 637.839)  # Lower right
 ], dtype=np.float32)
 OBJECT_POINTS = np.array([
-    (0.973203478362, 0.53220275843),  # Upper left (X, Y, Z)
-    (0.335471638151, 0.498266287581),  # Lower left
-    (1.00756553233, -0.487021132297),  # Upper right
-    (0.375112755416, -0.499386870421)  # Lower right
+    (0.972906622465, 0.513606512261),  # Upper left (X, Y)
+    (0.346150216655, 0.491463406542),  # Lower left
+    (0.996763251255, -0.512213883443),  # Upper right
+    (0.372487505071, -0.513767038756)  # Lower right
 ], dtype=np.float32)

src/helpers/find_contour_angle.py
@@ -14,54 +14,6 @@ Original code:
 http://nicky.vanforeest.com/misc/fitEllipse/fitEllipse.html
 """
 
-def fitEllipse(cont):
-    """
-    Return a list with center coordinates, axis length and 
-    angle of an ellipse.
-
-    Parameters
-    ----------
-    cont : Numpy array 
-        Array of contour points
-
-    Returns
-    ------- 
-    out : List
-        List with center coordinates, axis lengths and angle
-    """
-    import numpy as np
-
-    x = cont[:, 0]
-    y = cont[:, 1]
-
-    x = x[:, None]
-    y = y[:, None]
-
-    D = np.hstack([x * x, x * y, y * y, x, y, np.ones(x.shape)])
-    S = np.dot(D.T, D)
-    C = np.zeros([6, 6])
-    C[0, 2] = C[2, 0] = 2
-    C[1, 1] = -1
-    E, V = np.linalg.eig(np.dot(np.linalg.inv(S), C))
-    n = np.argmax(np.abs(E))
-    a = V[:, n]
-
-    # Fit ellipse
-    b, c, d, f, g, a = a[1] / 2., a[2], a[3] / 2., a[4] / 2., a[5], a[0]
-    num = b*b-a*c
-    cx = (c * d - b * f) / num
-    cy = (a * f - b * d) / num
-
-    angle = 0.5 * np.arctan(2 * b / (a - c)) * 180 / np.pi
-    up = 2 * (a * f * f + c * d * d + g * b * b - 2 * b * d * f - a * c * g)
-    down1 = (b * b - a * c) * ((c - a) * np.sqrt(1 + 4 * b * b / ((a - c) * (a - c))) - (c + a))
-    down2 = (b * b - a * c) * ((a - c) * np.sqrt(1 + 4 * b * b / ((a - c) * (a - c))) - (c + a))
-    a = np.sqrt(abs(up / down1))
-    b = np.sqrt(abs(up / down2))
-
-    params = [cx, cy, a, b, angle]
-    return params
-
 def fitAngleToBaxter(angle):
     """
     Return an angle fitted to use with Baxter. Returned angle goes from
@@ -77,49 +29,8 @@ def fitAngleToBaxter(angle):
     new_angle : ```float```
         Angle transformed into Baxter's yaw.
     """ 
-    return -179.9 + angle
-
-def getContourAnglex(mask, angle_type='deg'):
-    """
-    Return the angle of a contour.
-
-    Parameters
-    ----------
-    masks : 2D array
-        2D array of mask values.
-    
-    angle_type : String
-        "rad" for radians or "deg" for degrees.
-    """
-    import sys
-    try:
-        sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
-    except:
-        pass        
-    import cv2
-    import numpy as np
-    from .const import ANGLE_OFFSET
-
-    # Mask Polygon
-    # Pad to ensure proper polygons for masks that touch image edges.
-    padded_mask = np.zeros((mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
-    padded_mask[1:-1, 1:-1] = mask
-    # Helps to find the contours from the mask
-    contours, _ = cv2.findContours(padded_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnt = contours[0]
-    # Fits an ellipse to the contours
-    params = fitEllipse(cnt.squeeze())     
-    print(params)
-    x, y, w, h, a = params
-
-    angle = 90 - a if (w < h) else -a
-    
-    print('Fitted', angle)
-
-    if angle_type == 'rad':
-        angle = np.deg2rad(angle)
-
-    return -angle
+        
+    return -90 + angle if angle <= 90 else -180 + (angle - 90)
 
 def getContourAngle(mask, angle_type='deg'):
     """
@@ -160,11 +71,11 @@ def getContourAngle(mask, angle_type='deg'):
 
     ellipse = cv2.fitEllipse(best)
     (c,y),(w,h),a = ellipse
-    print(a)
-    angle = 90 - a if a <= 90 else 180 -  a
-    print("first angle: ", angle)
+    print("Ellipse = ", a)    
+    angle = 90 + a if a <= 90 else a - 90
+    print("Real = ", angle)
     angle = fitAngleToBaxter(angle)
-    print("Angle for baxter = ", angle)
+    print("Baxter = ", angle)
     if angle_type == 'rad':
         angle = np.deg2rad(angle)
 

src/pick_and_place.py
@@ -40,10 +40,10 @@ def calculateObjPose(obj_to_pick, u, v, orientation):
 
     if obj_to_pick is None:
         print 'No object to pick. Check objects_detected.'
-        return 0, 0, 0
+        return
     elif obj_to_pick not in const.OBJECTS:
         print 'The object to pick is not registered. Check OBJECTS.'
-        return 0, 0, 0
+        return
     else:
         z_baxter = const.Z_TABLE_BAXTER + const.OBJECTS[obj_to_pick] - const.Z_GRIP_DEPTH    
         
@@ -73,9 +73,12 @@ def calculateObjPose(obj_to_pick, u, v, orientation):
     # print 'First quaternions, ', quaternions
     # return x_baxter, y_baxter, z_baxter, quaternions
     # #################################
-    x_baxter, y_baxter = solve_pnp.getXYPoint(u, v)
-    print orientation
-    quaternions = quaternion_from_euler(176.0, 0.0, -orientation)
+    x_baxter, y_baxter = solve_pnp.getXYPoint(u, v)    
+    print 'x=', np.deg2rad(176.0)
+    print 'z=', orientation
+    quaternions = quaternion_from_euler(np.deg2rad(176.0), 0.0, orientation)
+    if quaternions[1] < 0:
+        quaternions[1] = -quaternions[1]
     return x_baxter, y_baxter, z_baxter, quaternions
     
 